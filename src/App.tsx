698a7fe3d25d0c5e9018bbb88938e93edc1d7aa5
hnobi <hnobi08@yahoo.com>
2025-05-21 10:31:47 +0100

    initial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <div className='text-cyan-500 text-3xl font-bold underline'>
+      Hello
+      <Button variant='destructive'> Hammed</Button> 
+    </div>
+  )
+}
+
+export default App
